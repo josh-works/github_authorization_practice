5947e39e75ae0b9d055f608858591804fe00deb6
Josh Thompson <thompsonjoshd@gmail.com>
2021-07-15 11:48:20 -0600

    Remove unnecessary views route?

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   devise_for :users
-  devise_for :views
   
   # devise_for :users, skip: [:sessions], controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 end
