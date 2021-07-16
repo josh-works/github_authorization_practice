13ebef10eb1e9accdc44e16e1e38855c292260db
Josh Thompson <thompsonjoshd@gmail.com>
2021-07-16 10:57:00 -0600

    prep user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
          
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-     user.email = auth.info.email
+     user.email    = auth.info.email
      user.password = Devise.friendly_token[0, 20]
-     user.name = auth.info.name   # assuming the user model has a name
-     user.image = auth.info.image # assuming the user model has an image
+     user.name     = auth.info.name   # assuming the user model has a name
+     user.image    = auth.info.image # assuming the user model has an image
+     user.nickname = auth.info.nickname
+     
      # If you are using confirmable and the provider(s) you use validate emails, 
      # uncomment the line below to skip the confirmation emails.
      # user.skip_confirmation!
@@ -20,9 +22,6 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
    super.tap do |user|
      if data = session["devise.github"] && session["devise.github_data"]["extra"]["raw_info"]
-       user.email = data["email"] if user.email.blank?
-     end
-     if data = session["devise.google_oauth2"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
        user.email = data["email"] if user.email.blank?
      end
    end
