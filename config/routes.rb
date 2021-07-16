b355a270a57baefe45336ed73f1a8c57bdabcbcb
Josh Thompson <thompsonjoshd@gmail.com>
2021-07-16 10:56:36 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  devise_for :users
   
-  # devise_for :users, skip: [:sessions], controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
