61cdc377b3e16315020cf8959485c5431217dfce
Josh Thompson <thompsonjoshd@gmail.com>
2021-07-21 22:54:37 -0600

    users can create brag entries

--- app/controllers/brag_entry_controller.rb
+++ app/controllers/brag_entry_controller.rb
@@ -2,4 +2,29 @@ class BragEntryController < ApplicationController
   def index
     @all_entries = current_user.brag_entries
   end
+  
+  def new
+    @brag_entry = BragEntry.new
+    respond_to do |format|
+      format.html { render :new, locals: { brag_entry: @brag_entry } }
+    end
+  end
+  
+  def create
+    @brag_entry = current_user.brag_entries.create(brag_params)
+    
+    if @brag_entry.save
+      redirect_to brag_entry_index_path
+    else
+      flash.alert "there was a problem"
+      redirect_to brag_entry_path, notice: flash[:notice]
+    end
+    
+  end
+  
+  private
+  
+  def brag_params
+    params.require(:brag_entry).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "brag_entry#index"
   resources :brag_entry
+  post "/brag_entry/new" => "brag_entry#create"
+  
+  root to: "brag_entry#index"
   
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
